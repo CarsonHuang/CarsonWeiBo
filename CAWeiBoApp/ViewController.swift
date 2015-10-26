6eea54407f2cb117d9df0a7b34a9ff7830af0bd1
Carson Huang <1277654102@qq.com>
2015-10-26 19:14:21 +0800

    Initial Commit

---
+++ CAWeiBoApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CAWeiBoApp
+//
+//  Created by  HuangCarson on 15/10/26.
+//  Copyright © 2015年 HuangCarson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
